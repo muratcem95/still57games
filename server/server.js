8e6c6dc6e343d6728fcd7d75d41fb8c733827b9b
muratcem95 <40249419+muratcem95@users.noreply.github.com>
2018-11-04 11:22:23 -0500

    email altered

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.get('/contactUs', (req, res) => {
 app.post('/contactUsForm', (req, res) => {
     var mailOptions = {
         from: 'muratcem95@gmail.com',
-        to: 'muratcem95@gmail.com',
+        to: 'still57games@gmail.com',
         subject: `Still57 Games: ${req.body.email}`,
         text: `Phone Model: ${req.body.phone_model}, Software Version: ${req.body.software_version}, Message: ${req.body.message}`
     };
