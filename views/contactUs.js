b1d094b8af4f353e0b964b16040307d5fb13b1a8
muratcem95 <40249419+muratcem95@users.noreply.github.com>
2018-11-11 17:03:52 -0500

    Form email validation

--- views/contactUs.js
+++ views/contactUs.js
@@ -30,25 +30,38 @@ $("#submit").click(function() {
     var email = $("#email").val();
     var message = $("#message").val();
     
-    if(name && email && message) {
-        $("#alert_success").html('<div class="alert alert-primary text-center alert-dismissible fade show" role="alert"><div id="space"></div><h5>Thank you <u>' + name + '</u> for Contacting Us!</h5><span>We will get back to you as soon as possible. :)</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
-        
-        name = $("#name").val("");
-        email = $("#email").val("");
-        message = $("#message").val("");
-        
-        $(".form-control").addClass("grey");
-        
-        $(".invalid-feedback").addClass("hide");
-    } else {
-        $("#alert_success").html('');
-        
-        $(".form-control").removeClass("grey");
-        
-        $(".invalid-feedback").removeClass("hide");
+    function validateEmail(em) {
+        var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+        return re.test(em);
+    };
+    
+    if(name && message) {
+        if(!validateEmail(email)) {
+            email.focus();
+            return false;
+        } else {
+            if(name && email && message) {
+                $("#alert_success").html('<div class="alert alert-primary text-center alert-dismissible fade show" role="alert"><div id="space"></div><h5>Thank you <u>' + name + '</u> for Contacting Us!</h5><span>We will get back to you as soon as possible. :)</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+
+                name = $("#name").val("");
+                email = $("#email").val("");
+                message = $("#message").val("");
+
+                $(".form-control").addClass("grey");
+
+                $(".invalid-feedback").addClass("hide");
+            } else {
+                $("#alert_success").html('');
+
+                $(".form-control").removeClass("grey");
+
+                $(".invalid-feedback").removeClass("hide");
+            };
+        };
     };
 }); 
-                    
+
+ 
                     
                              
                              
